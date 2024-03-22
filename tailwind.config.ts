0d1a651eedc2d4136afee742aa8865e17754639b
hollali <dheztinykartel@gmail.com>
2024-03-22 04:16:41 +0000

    "Add custom theme to Tailwind config with DaisyUI"
    This commit adds a custom theme to the Tailwind config file using the DaisyUI plugin. The theme includes various color properties such as primary, secondary, accent, neutral, base-100, info, success, warning, and error. Each property has a specific hexadecimal color code assigned to it.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,5 +7,49 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+
+
+"primary": "#0043ff",
+        
+
+
+"secondary": "#96ac00",
+        
+
+
+"accent": "#9e4500",
+        
+
+
+"neutral": "#100400",
+        
+
+
+"base-100": "#1b2b36",
+        
+
+
+"info": "#0068cf",
+        
+
+
+"success": "#75c900",
+        
+
+
+"warning": "#d19c00",
+        
+
+
+"error": "#ff597a",
+        },
+      },
+    ],
+  },
 };
 export default config;
