f467ca25ce51fc53b6f706b26cd3f211cccb5a8c
hollali <dheztinykartel@gmail.com>
2024-04-11 15:37:04 +0000

    "Add heroImage schema type to schemaTypes array

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,4 +1,5 @@
 import category from "./category";
+import heroImage from "./heroImage";
 import products from "./products";
 
-export const schemaTypes = [products,category]
+export const schemaTypes = [products,category,heroImage]
