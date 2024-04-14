8d84dc8c252e541dcc64d21d4d31861fbc0bc7b9
hollali <dheztinykartel@gmail.com>
2024-04-14 11:18:08 +0000

    Added postcss-preset-env with stage 1 features and nesting-rules
    This commit adds the postcss-preset-env plugin to the PostCSS configuration with stage 1 features and enables the nesting-rules

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,11 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    require('postcss-preset-env')({
+      stage: 1,
+      features: {
+        'nesting-rules': true,
+      },
+    }),
   },
 };
