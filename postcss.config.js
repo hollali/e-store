da5515929cf2c742c24e7b2331648e2c4dd8324c
hollali <dheztinykartel@gmail.com>
2024-04-14 11:19:47 +0000

    Remove postcss-preset-env and nesting-rules feature
    This commit removes the `postcss-preset-env` plugin and the `nesting-rules` feature from the PostCSS configuration. The `stage

--- postcss.config.js
+++ postcss.config.js
@@ -2,11 +2,5 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    require('postcss-preset-env')({
-      stage: 1,
-      features: {
-        'nesting-rules': true,
-      },
-    }),
   },
 };
