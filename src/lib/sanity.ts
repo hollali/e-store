e54a4270bb30124864013bbb653a3d554e058e46
hollali <dheztinykartel@gmail.com>
2024-04-14 12:10:02 +0000

    Update Sanity client configuration and add image URL builder
    * Replace placeholders with actual project ID and API version
    * Enable use of CDN
    * Add image URL builder function for generating image URLs from Sanity asset sources

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,8 +1,15 @@
 import {createClient} from 'next-sanity'
+import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-    projectId: '',
+    projectId: 'rhvfdi2z',
     dataset: 'production',
-    apiVersion: '',
+    apiVersion: '2024-04-14',
     useCdn: true,
 });
+
+const builder = imageUrlBuilder(client)
+
+export function urlFor(source:any){
+    return builder.image(source);
+}
