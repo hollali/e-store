1e714e5f1eed64a12869485e4328005158da8891
hollali <dheztinykartel@gmail.com>
2024-04-15 13:07:46 +0000

    "Replace header with Hero component"
    This commit removes the header section, which contained a link to the homepage, and adds a new Hero component. The header section had a border at the bottom, while the Hero component is placed inside

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 import Image from "next/image";
 import Link from 'next/link';
+import Hero from '../components/hero';
 
 
 export default function Home() {
   return (
-    <header className="mb-8 border-b">
-      <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-2-7xl">
-        <Link href="/">
-          <h1 className="text-2xl font-bold">E-Store</h1>
-        </Link>
-      </div>
-    </header>
+    <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
+      <Hero/>
+    </div>
   );
 }
