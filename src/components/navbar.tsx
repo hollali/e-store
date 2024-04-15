5179b243c3e23fe3fbf6b93ac90c3b4f472deb29
hollali <dheztinykartel@gmail.com>
2024-04-15 10:00:11 +0000

    "Update Navbar component to use Next Link and add branding"
    This commit updates the Navbar component to use the Next Link component for routing and adds branding to the header with a link to the homepage and a logo.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,16 @@
+import Link from 'next/link';
+
 export default function Navbar() {
     return (
-            <div>
-                <h1>Hello</h1>
+        <header className="mb-8 border-b">
+            <div className="flex items-center justify-between mx-auto max-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
+                <Link href="/">
+                    <h1 className="text-2xl font-bold">
+                        E-<span className='text-primary'>Store</span>
+                    </h1>
+                </Link>
             </div>
+        </header>
     );
 }
+
