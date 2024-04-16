d631c6ecb0e2c0820d4f066f5e8c1bb2295175a3
hollali <dheztinykartel@gmail.com>
2024-04-16 23:14:05 +0000

    "Add Newest component displaying newest products on homepage"
    This commit adds a new component, "Newest", to the homepage which displays the newest products in the database. The component is implemented as an async function that fetches

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Link from 'next/link';
 import Hero from '../components/hero';
+import Newest from "@/components/newest";
 
 
 export default function Home() {
   return (
     <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
       <Hero/>
+      <Newest/>
     </div>
   );
 }
--- src/components/newest.tsx
+++ src/components/newest.tsx
@@ -1,3 +1,8 @@
+import { simplifiedProduct } from "@/app/interface";
+import { client } from "@/lib/sanity";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
 async function getData() {
     const query = `*[_type == "product"][0...4] | order(_createdAt desc) {
         _id,
@@ -7,6 +12,29 @@ async function getData() {
         "categoryName": category->name,
         "imageUrl": images[0].assets->url
     }`;
+
+    const data = await client.fetch(query);
+
+    return data;
 }
 
-export default function Newest () {}
+export default async function Newest () {
+    const data : simplifiedProduct[] = await getData();
+
+    return (
+        <div className="bg-white">
+            <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+                <div className="flex justify-between items-center">
+                    <h2 className="text-2xl font-bold tracking-tight text-gray-900">Our Newest Products</h2>
+                <Link className="text-primary flex items-center gap-x-1" href="/all">
+                    See All{""}
+                    <span>
+                        <ArrowRight/>
+                    </span>
+                </Link>    
+                </div>
+                <div className=""></div>
+            </div>
+        </div>
+    )
+}
