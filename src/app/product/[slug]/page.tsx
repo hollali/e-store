83705ca1f4e8d68cbaaf148d9e50ca2663db2556
hollali <dheztinykartel@gmail.com>
2024-04-17 16:37:30 +0000

    "Change product link in Newest component from '/products/:slug' to '/product/:slug'"

---
+++ src/app/product/[slug]/page.tsx
@@ -0,0 +1,3 @@
+export default function ProductPage() {
+    return <h1>Hello From the Product Page</h1>;
+}
--- src/components/newest.tsx
+++ src/components/newest.tsx
@@ -49,7 +49,7 @@ export default async function Newest () {
                             <div className="mt-4 flex justify-between">
                                 <div>
                                     <h3 className="text-sm text-gray-700">
-                                        <Link href={`/products/${product.slug}`}>
+                                        <Link href={`/product/${product.slug}`}>
                                             {product.name}
                                         </Link>
                                     </h3>
