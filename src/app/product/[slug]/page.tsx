442f0e2aa597412836f1e13dc938cd76f4842b71
hollali <dheztinykartel@gmail.com>
2024-04-18 12:03:27 +0000

    Add async function `getData` to fetch product data in `page.tsx`
    This commit introduces a new async function `getData` to fetch product data in `page.tsx`. The function queries for a product with a matching

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,3 +1,14 @@
+async function getData(slug: string){
+    const query = `*[_type == "product" && slug.current == "${slug}[0]{
+        _id,
+          images,
+          price,
+          name,
+          description,
+          "slug": slug.current,
+          "categoryName": category->name,
+      }`;
+}
 export default function ProductPage() {
     return <h1>Hello From the Product Page</h1>;
 }
