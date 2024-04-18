c86d5c45604144150674044fcd5133ce0b7ee92d
hollali <dheztinykartel@gmail.com>
2024-04-18 18:55:50 +0000

    "Import Sanity client and fetch product data in getData function"
    This commit adds an import statement for the Sanity client and modifies the getData function to fetch product data using the client. The function takes a slug parameter and

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { client } from '@/lib/sanity';
 async function getData(slug: string){
     const query = `*[_type == "product" && slug.current == "${slug}[0]{
         _id,
@@ -8,6 +9,8 @@ async function getData(slug: string){
           "slug": slug.current,
           "categoryName": category->name,
       }`;
+
+    const data = await client.fetch(query)
 }
 export default function ProductPage() {
     return <h1>Hello From the Product Page</h1>;
