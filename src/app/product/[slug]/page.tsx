b5a8a38e88806ab1227e4ccc90ecdd70875fb442
hollali <dheztinykartel@gmail.com>
2024-04-18 21:38:26 +0000

    "Add ImageGallery component to product page"
    This commit adds the `ImageGallery` component to the product page and passes the `data.images` prop to it. The existing `h1` element is replaced with the new

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,7 +1,9 @@
 import { client } from '@/lib/sanity';
 import { fullProduct } from '@/app/interface';
+import ImageGallery from '@/components/imageGallery';
+
 async function getData(slug: string){
-    const query = `*[_type == "product" && slug.current == "${slug}[0]{
+    const query = `*[_type == "product" && slug.current == "${slug}"][0]{
         _id,
           images,
           price,
@@ -18,7 +20,15 @@ export default async function ProductPage({
     params,
 }:{
     params:{slug:string};
-}) {
+} ) {
     const data:fullProduct = await getData(params.slug);
-    return <h1>Hello From the Product Page</h1>;
+    return (
+        <div className="bg-white">
+            <div className='mx-auto max-w-screen-xl px-4 md:px-8'>
+                <div className='grid gap-8 md:grid-cols-2'>
+                    <ImageGallery images={data.images}/>
+                </div>
+            </div>
+        </div>
+    );
 }
---
+++ src/components/imageGallery.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { urlFor } from "@/lib/sanity";
+
+interface iAppProps {
+    images: any;
+}
+export default function ImageGallery({images}: iAppProps){
+    return(
+        <div className="grid gap-4 lg:grid-cols-5">
+            <div className="order-last flex gap-4 lg:order-none lg:flex-col">
+                {images.map((image:any, idx:any)=>(
+                    <div key={idx} className="overflow-hidden rounded-lg bg-gray-100">
+                        <Image 
+                        src={urlFor(image).url()} 
+                        width={200}
+                        height={200} 
+                        alt="photo"
+                        className="h-full w-full object-cover object-center cursor-pointer"
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
