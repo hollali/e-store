c176146c8a508f29b9398f2458f36a60b82c14b1
hollali <dheztinykartel@gmail.com>
2024-04-19 07:30:56 +0000

    "Update page.tsx: add whitespace, correct type annotation in params"
    Explanation:
    
    * The diff adds a single whitespace character in the getData function and another one before the return statement in the Product

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -14,20 +14,20 @@ async function getData(slug: string){
       }`;
 
     const data = await client.fetch(query);
-    
+
     return data;
 }
 export default async function ProductPge({
     params,
 }:{
-    params:{slug:string};
+    params: { slug: string };
 }) {
     const data:fullProduct = await getData(params.slug);
     return (
         <div className="bg-white">
             <div className='mx-auto max-w-screen-xl px-4 md:px-8'>
                 <div className='grid gap-8 md:grid-cols-2'>
-                    <ImageGallery images={data.images}/>
+                    <ImageGallery images={data.images} />
                 </div>
             </div>
         </div>
