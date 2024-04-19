65bc328eaf8257b8fb9e593ea95083998ebfa481
hollali <dheztinykartel@gmail.com>
2024-04-19 06:59:32 +0000

    Change images map function to use arrow function syntax
    This commit updates the syntax of the images map function in the ImageGallery component to use arrow function syntax. The functional component is now defined as:
    
    {images.map((image:

--- src/components/imageGallery.tsx
+++ src/components/imageGallery.tsx
@@ -8,7 +8,7 @@ export default function ImageGallery({images}: iAppProps){
     return(
         <div className="grid gap-4 lg:grid-cols-5">
             <div className="order-last flex gap-4 lg:order-none lg:flex-col">
-                {images.map((image:any, idx:any)=>(
+                {images.map((image:any, idx:any) => (
                     <div key={idx} className="overflow-hidden rounded-lg bg-gray-100">
                         <Image 
                         src={urlFor(image).url()} 
