cfd44457e0366187d5e47ae9214cc317adf9922b
hollali <dheztinykartel@gmail.com>
2024-04-21 12:17:51 +0000

    "Update product page with new components and functionality; remove item counter from navbar"
    This commit includes updates to the product page, with the addition of new components such as image gallery, product details, and CTA buttons. It also includes

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import { client } from '@/lib/sanity';
 import { fullProduct } from '@/app/interface';
 import ImageGallery from '@/components/imageGallery';
+import { Button} from "@/components/ui/button";
+import { Star, Truck } from 'lucide-react';
 
 async function getData(slug: string){
     const query = `*[_type == "product" && slug.current == "${slug}"][0]{
@@ -23,11 +25,46 @@ export default async function ProductPge({
     params: { slug: string };
 }) {
     const data:fullProduct = await getData(params.slug);
+    const cedisSign = '\u20B5';
     return (
         <div className="bg-white">
             <div className='mx-auto max-w-screen-xl px-4 md:px-8'>
                 <div className='grid gap-8 md:grid-cols-2'>
                     <ImageGallery images={data.images} />
+                    <div className='md:py-8'>
+                        <div className="mb-2 md:mb-3">
+                            <span className='mb-0.5 inline-block text-gray-500'>{data.categoryName}</span>
+                            <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">
+                                {data.name}
+                            </h2>
+                        </div>
+                        <div className='mb-6 flex item center gap-3 md:mb-10'>
+                            <Button className='rounded-full gap-x-2'>
+                                <span className='text-sm'>4.5</span>
+                                <Star className='h-5 w-5'/>
+                            </Button>
+                            <span className="text-sm text-gray-500 transition duration">56 Ratings</span>
+                        </div>
+                        <div className='mb-4'>
+                            <div className='flex items-end gap-2'>
+                                <span className="text-xl font-bold text-gray-800 md:text-2xl">{cedisSign} {data.price}</span>
+                                <span className="mb-0.5 text-red-500 line-through">{cedisSign} {data.price + 20}</span>
+                            </div>
+                            <span className='text-sm text-gray-500'>
+                                Incl. Vat. shipping is different</span>
+                        </div>
+                        <div className="mb-6 flex items-center gap-2 text-gray-500">
+                            <Truck className='h-6 w-6'/>
+                            <span className="text-sm">1-3 Days Shipping</span>
+                        </div>
+                        <div className='flex gap-2.5'>
+                            <Button>Add to Cart</Button>
+                            <Button variant={"secondary"}>Checkout</Button>
+                        </div>
+                        <p className='mt-12 text-gray-500 tracking-wide'>
+                            {data.description}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/imageGallery.tsx
+++ src/components/imageGallery.tsx
@@ -1,10 +1,17 @@
+"use client";
 import Image from "next/image";
 import { urlFor } from "@/lib/sanity";
+import { useState } from "react";
 
 interface iAppProps {
     images: any;
 }
 export default function ImageGallery({images}: iAppProps){
+    const [bigImage, setBigImage] = useState(images[0]);
+
+    const handleSmallImageClick = (image: any) => {
+        setBigImage(image);
+    }
     return(
         <div className="grid gap-4 lg:grid-cols-5">
             <div className="order-last flex gap-4 lg:order-none lg:flex-col">
@@ -16,9 +23,22 @@ export default function ImageGallery({images}: iAppProps){
                         height={200} 
                         alt="photo"
                         className="h-full w-full object-cover object-center cursor-pointer"
+                        onClick={()=>handleSmallImageClick(image)}
                         />
+                        
                     </div>
                 ))}
+            </div>
+            <div className="relative overflow-hidden rounded-lg bg-gray-100 lg:col-span-4">
+                <Image 
+                src={urlFor(bigImage).url()} 
+                alt="Photo" 
+                width={500} 
+                height={500} 
+                className="h-full w-full object-cover object-center"
+                />
+                <span className="absolute left-0 top-0 rounded-br-lg bg-red-500 px-3 py-1.5 text-sm uppercase tracking-wider text-white">SALE</span>
+
             </div>
         </div>
     );
