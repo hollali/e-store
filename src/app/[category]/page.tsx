d512e89f98d4c95c01112f7c7ac6702222fb8d9e
hollali <dheztinykartel@gmail.com>
2024-04-21 20:47:41 +0000

    Created page.tsx in category folder for categories

---
+++ src/app/[category]/page.tsx
@@ -0,0 +1,60 @@
+import { client } from "@/lib/sanity";
+import { simplifiedProduct } from "../interface";
+import Image from "next/image";
+import Link from "next/link";
+
+async function getData(category : string) {
+    const query = `*[_type == "product" && category->name == "${category}"]{
+  _id,
+    "imageUrl": images[0].asset->url,
+    price,
+    name,
+    "slug":slug.current,
+    "categoryName":category->name
+}`;
+const data = await client.fetch(query);
+return data;
+}
+export default async function CategoryPage({
+  params,
+}:{params: { category: string };
+}){
+  const data:simplifiedProduct[] = await getData(params.category); 
+  const cedisSign = '\u20B5';
+  return (
+    <div className="bg-white">
+            <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6  lg:max-w-7xl lg:px-8">
+                <div className="flex justify-between items-center">
+                    <h2 className="text-2xl font-bold tracking-tight text-gray-900">Our Products for {params.category}</h2>  
+                </div>
+                <div className="mt-6 grid grid-cols-1 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                    {data.map((product) => (
+                        <div key={product._id} className="group relative">
+                            <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
+                                <Image 
+                                src={product.imageUrl} 
+                                alt="Product image" 
+                                className="w-full h-full object-cover object-center  lg:h-full lg:w-full"
+                                width={300}
+                                height={300}/>
+                            </div>
+                            <div className="mt-4 flex justify-between">
+                                <div>
+                                    <h3 className="text-sm text-primary font-semibold">
+                                        <Link href={`/product/${product.slug}`}>
+                                            {product.name}
+                                        </Link>
+                                    </h3>
+                                    <p className="mt-1 text-sm text-gray-500">
+                                        {product.categoryName}
+                                    </p>
+                                </div>
+                                <p className="text-sm font-medium text-gray-900">{cedisSign} {product.price}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+  )
+}
