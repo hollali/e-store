365a010bdafee69ac6ba210d72d8c13541648397
hollali <dheztinykartel@gmail.com>
2024-04-24 11:13:10 +0000

    "Add ShoppingCartModal to app layout"
    This commit adds the `ShoppingCartModal` component to the app's layout, inside the `CartProvider` component. The `ShoppingCartModal` component was previously not present

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import CartProvider from "@/components/provider";
+import ShoppingCartModal from "@/components/ShoppingCartModal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <CartProvider>
           <Navbar/>
+          <ShoppingCartModal/>
           {children}
           <Footer/>
         </CartProvider>   
