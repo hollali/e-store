ee10ed4d478f7b1ff470029374be6e541b973232
hollali <dheztinykartel@gmail.com>
2024-05-30 02:41:35 +0000

    Change the layout for mobile

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -3,75 +3,91 @@ import { simplifiedProduct } from "../interface";
 import Image from "next/image";
 import Link from "next/link";
 
-async function getData(category : string) {
-    const query = `*[_type == "product" && category->name == "${category}"]{
-  _id,
+async function getData(category: string) {
+  const query = `*[_type == "product" && category->name == "${category}"]{
+    _id,
     "imageUrl": images[0].asset->url,
     price,
     name,
-    "slug":slug.current,
-    "categoryName":category->name
-}`;
+    "slug": slug.current,
+    "categoryName": category->name
+  }`;
 
-
-const data = await client.fetch(query);
-return data;
+  const data = await client.fetch(query);
+  return data;
 }
+
 export const dynamic = "force-dynamic";
 
 export default async function CategoryPage({
   params,
-}:{params: { category: string };
-}){
-  const data:simplifiedProduct[] = await getData(params.category); 
+}: {
+  params: { category: string };
+}) {
+  const data: simplifiedProduct[] = await getData(params.category);
   const cedisSign = '\u20B5';
+  
   return (
     <div className="bg-white">
-            <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6  lg:max-w-7xl lg:px-8">
-                <div className="flex justify-between items-center">
-                    <h2 className="text-2xl font-bold tracking-tight text-gray-900">Our Products for {params.category}</h2>  
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="flex justify-between items-center">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+            Our Products for {params.category}
+          </h2>
+        </div>
+        <div className="mt-6 grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {data.map((product) => (
+            <div key={product._id} className="group relative">
+              <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                <Image
+                  src={product.imageUrl}
+                  alt="Product image"
+                  className="w-full h-full object-cover object-center lg:h-full lg:w-full"
+                  width={300}
+                  height={300}
+                />
+              </div>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h6 className="text-sm text-primary font-semibold">
+                    <Link href={`/product/${product.slug}`}>
+                      {product.name}
+                    </Link>
+                  </h6>
+                  <p className="mt-1 text-sm text-gray-500">
+                    {product.categoryName}
+                  </p>
                 </div>
-                <div className="mt-6 grid grid-cols-1 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                    {data.map((product) => (
-                        <div key={product._id} className="group relative">
-                            <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
-                                <Image 
-                                src={product.imageUrl} 
-                                alt="Product image" 
-                                className="w-full h-full object-cover object-center  lg:h-full lg:w-full"
-                                width={300}
-                                height={300}/>
-                            </div>
-                            <div className="mt-4 flex justify-between">
-                                <div>
-                                    <h3 className="text-sm text-primary font-semibold">
-                                        <Link href={`/product/${product.slug}`}>
-                                            {product.name}
-                                        </Link>
-                                    </h3>
-                                    <p className="mt-1 text-sm text-gray-500">
-                                        {product.categoryName}
-                                    </p>
-                                </div>
-                                <p className="text-sm font-medium text-gray-900">{cedisSign} {product.price}</p>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                <p className="text-sm font-medium text-gray-900">
+                  {cedisSign} {product.price}
+                </p>
+              </div>
             </div>
-            <div className="flex flex-col items-center justify-between gap-8 md:flex-row">
-    <div className="flex h-12 w-64 divide-x overflow-hidden rounded-lg border">
-        <Link href="/Men" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-between gap-8 md:flex-row">
+        <div className="flex h-12 w-64 divide-x overflow-hidden rounded-lg border">
+          <Link
+            href="/Men"
+            className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200"
+          >
             Men
-        </Link>
-        <Link href="/Women" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">
+          </Link>
+          <Link
+            href="/Women"
+            className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200"
+          >
             Women
-        </Link>
-        <Link href="/Children" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">
+          </Link>
+          <Link
+            href="/Children"
+            className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200"
+          >
             Children
-        </Link>
+          </Link>
+        </div>
+      </div>
     </div>
-</div>
-</div>
- );
+  );
 }
--- src/components/newest.tsx
+++ src/components/newest.tsx
@@ -4,67 +4,70 @@ import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 
-
 async function getData() {
-    const query = `*[_type == "product"][0...4] | order(_createdAt desc) {
-        _id,
-        price,
-        name,
-        "slug":slug.current,
-        "categoryName": category->name,
-        "imageUrl": images[0].asset->url
-    }`;
+  const query = `*[_type == "product"][0...4] | order(_createdAt desc) {
+    _id,
+    price,
+    name,
+    "slug": slug.current,
+    "categoryName": category->name,
+    "imageUrl": images[0].asset->url
+  }`;
 
-    const data = await client.fetch(query);
+  const data = await client.fetch(query);
 
-    return data;
+  return data;
 }
 
-export default async function Newest () {
-    const data : simplifiedProduct[] = await getData();
-    const cedisSign = '\u20B5';
-    return (
-        <div className="bg-white">
-            <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                <div className="flex justify-between items-center">
-                    <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-                        Our Newest Products
-                    </h2>
-                <Link className="text-primary flex items-center gap-x-1" href="all">
-                    See All{""}
-                    <span>
-                        <ArrowRight/>
-                    </span>
-                </Link>    
-                </div>
-                <div className="mt-6 grid grid-cols-1 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                    {data.map((product) => (
-                        <div key={product._id} className="group relative">
-                            <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
-                                <Image 
-                                src={product.imageUrl} 
-                                alt="Product image" 
-                                className="w-full h-full object-cover object-center  lg:h-full lg:w-full"
-                                width={300}
-                                height={300}/>
-                            </div>
-                            <div className="mt-4 flex justify-between">
-                                <div>
-                                    <h3 className="text-sm text-primary font-semibold">
-                                        <Link href={`/product/${product.slug}`}>
-                                            {product.name}
-                                        </Link>
-                                    </h3>
-                                    <p className="mt-1 text-sm text-gray-500">
-                                        {product.categoryName}
-                                    </p>
-                                </div>
-                                <p className="text-sm font-medium text-gray-900">{cedisSign} {product.price}</p>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
+export default async function Newest() {
+  const data: simplifiedProduct[] = await getData();
+  const cedisSign = '\u20B5';
+  
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <div className="flex justify-between items-center">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+            Our Newest Products
+          </h2>
+          <Link className="text-primary flex items-center gap-x-1" href="all">
+            See All{""}
+            <span>
+              <ArrowRight />
+            </span>
+          </Link>
         </div>
-    );
+        <div className="mt-6 grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {data.map((product) => (
+            <div key={product._id} className="group relative">
+              <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                <Image
+                  src={product.imageUrl}
+                  alt="Product image"
+                  className="w-full h-full object-cover object-center lg:h-full lg:w-full"
+                  width={300}
+                  height={300}
+                />
+              </div>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-primary font-semibold">
+                    <Link href={`/product/${product.slug}`}>
+                      {product.name}
+                    </Link>
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">
+                    {product.categoryName}
+                  </p>
+                </div>
+                <p className="text-sm font-medium text-gray-900">
+                  {cedisSign} {product.price}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
