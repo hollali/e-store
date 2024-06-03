6cccd2819cffea452ea70d16511fe1eca8ed5f92
hollali <dheztinykartel@gmail.com>
2024-06-03 17:49:31 +0000

    mobile layout

--- src/app/all/page.tsx
+++ src/app/all/page.tsx
@@ -29,7 +29,7 @@ export default async function AllProducts() {
         </h2>
       </div>
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <div className="mt-6 grid grid-cols-2 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <div className="mt-6 grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {data.map((product) => (
             <div key={product._id} className="group relative">
               <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
