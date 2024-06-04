aaaac0cb548fc8001c7a32f9c01d0a463600736c
hollali <dheztinykartel@gmail.com>
2024-06-04 17:30:35 +0000

    centered h2 element

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -30,11 +30,11 @@ export default async function CategoryPage({
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="flex justify-between items-center">
+      <div className="flex justify-center items-center h-full">
           <h2 className="text-2xl font-bold tracking-tight text-gray-900">
             Our Products for {params.category}
           </h2>
-        </div>
+      </div>
         <div className="mt-6 grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {data.map((product) => (
             <div key={product._id} className="group relative">
