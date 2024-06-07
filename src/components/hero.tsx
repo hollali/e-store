1c03b3e0f0f05efe4b5947d9746aac32691605fb
Hollali Kelvin <87954725+hollali@users.noreply.github.com>
2024-06-07 15:02:29 +0000

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,20 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
-import { client ,urlFor } from "../lib/sanity";
+import { client, urlFor } from "../lib/sanity";
 
 async function getData() {
-    const query = "*[_type == 'heroImage'][0]"
+    const query = "*[_type == 'heroImage'][0]";
 
     const data = await client.fetch(query);
 
     return data;
 }
+
 export default async function Hero() {
-    const data = await getData()
+    const data = await getData();
     return (
-        <section className="mx-auto max-w-2xl px-4 sm:pb-6 lg:max-w-7xl lg:px-8">
+        <section className="mx-auto mt-16 max-w-2xl px-4 sm:pb-6 lg:max-w-7xl lg:px-8">
             <div className="mb-8 flex flex-wrap justify-between md:mb-16">
-                <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0  lg:w-1/3 lg:pb-24 lg:pt-48">
+                <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
                     <h1 className="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8 md:text-6xl">
                         Top Fashion for an Affordable Price!
                     </h1>
@@ -26,22 +27,23 @@ export default async function Hero() {
                 <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
                     <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
                         <Image 
-                        src={urlFor(data.image1).url()}
-                        alt="Great Photo"
-                        className="h-full w-full object-cover object-center"
-                        width = {500}
-                        height = {500}
-                        priority
+                            src={urlFor(data.image1).url()}
+                            alt="Great Photo"
+                            className="h-full w-full object-cover object-center"
+                            width={500}
+                            height={500}
+                            priority
                         />
                     </div>
                     <div className="overflow-hidden rounded-lg bg-gray-100 shadow-lg">
                         <Image 
-                        src={urlFor(data.image2).url()}
-                        alt="Great Photo"
-                        className="h-full w-full object-cover object-center"
-                        width={500}
-                        height={500}
-                        priority/>
+                            src={urlFor(data.image2).url()}
+                            alt="Great Photo"
+                            className="h-full w-full object-cover object-center"
+                            width={500}
+                            height={500}
+                            priority
+                        />
                     </div>
                 </div>
             </div>
