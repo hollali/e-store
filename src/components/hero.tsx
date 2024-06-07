7d6adc1d9bcfc274e3504a7d5907a93536c1c0b6
Hollali Kelvin <87954725+hollali@users.noreply.github.com>
2024-06-07 15:29:13 +0000

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -13,7 +13,7 @@ async function getData() {
 export default async function Hero() {
     const data = await getData();
     return (
-        <section className="mx-auto mt-20 max-w-2xl px-4 sm:pb-6 lg:max-w-7xl lg:px-8">
+        <section className="mx-auto mt-24 max-w-2xl px-4 sm:pb-6 lg:max-w-7xl lg:px-8">
             <div className="mb-8 flex flex-wrap justify-between md:mb-16">
                 <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
                     <h1 className="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8 md:text-6xl">
