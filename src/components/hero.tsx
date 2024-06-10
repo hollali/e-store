f80f54dc5da401a746eb7abce623c7c3739cfe44
Hollali Kelvin <87954725+hollali@users.noreply.github.com>
2024-06-10 10:10:25 +0000

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -56,7 +56,7 @@ export default async function Hero() {
                         Women
                     </Link>
                     <Link href="/Accessories" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">
-                        Accessories
+                        Trimmings
                     </Link>
                 </div>
             </div>
