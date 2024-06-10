1168f241387babf4c3d05adb053bdf7f960e3ce7
Hollali Kelvin <87954725+hollali@users.noreply.github.com>
2024-06-10 09:55:22 +0000

    Update page.tsx

--- src/app/all/page.tsx
+++ src/app/all/page.tsx
@@ -80,10 +80,10 @@ export default async function AllProducts() {
               Women
             </Link>
             <Link
-              href="/Children"
+              href="/Accessories"
               className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200"
             >
-              Children
+              Accessories
             </Link>
           </div>
         </div>
