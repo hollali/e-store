5e1033878f4ede6d72b25d29532f3aa2e4c3faf2
Hollali Kelvin <87954725+hollali@users.noreply.github.com>
2024-06-13 19:44:55 +0000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import CartProvider from "@/components/provider";
 import ShoppingCartModal from "@/components/shoppingCartModal";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,9 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <CartProvider>
           <Navbar />
           <ShoppingCartModal />
           {children}
+        </CartProvider>
         <Footer/>
       </body>
     </html>
