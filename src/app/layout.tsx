4df99e3088b365ea98fb8caf366255d14e559fc2
Hollali Kelvin <87954725+hollali@users.noreply.github.com>
2024-06-15 18:47:03 +0000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import CartProvider from "@/components/provider";
 import ShoppingCartModal from "@/components/shoppingCartModal";
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,13 +21,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="AfricVogue" />
+        <meta property="og:description" content="Discover the latest trends and timeless styles at AfricVouge" />
+        <meta property="og:image" content="https://www.africvouge.com/path-to-your-logo-image.jpg" />
+        <meta property="og:url" content="https://www.africvouge.com" />
+        <meta property="og:type" content="website" />
+        <meta name="robots" content="index, follow" />
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify({
+          "@context": "https://schema.org",
+          "@type": "Organization",
+          "url": "https://www.africvouge.com",
+          "logo": "https://www.africvouge.com/path-to-your-logo-image.jpg"
+        }) }} />
+      </Head>
       <body className={inter.className}>
         <CartProvider>
           <Navbar />
           <ShoppingCartModal />
           {children}
         </CartProvider>
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
