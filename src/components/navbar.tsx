e92d8b7cdf011d3e26495f736d368a4e1e0d0685
Hollali Kelvin <87954725+hollali@users.noreply.github.com>
2024-07-03 09:15:23 +0000

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -79,11 +79,11 @@ export default function Navbar() {
             </div>
           ))}
         </nav>
-        <div className="hidden lg:flex items-center divide-x border-r sm:border-l">
+        <div className="flex items-center divide-x border-r sm:border-l">
           <Button
             variant="outline"
             onClick={() => handleCartClick()}
-            className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none relative mr-4" // Added margin right
+            className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none relative mr-4"
           >
             <ShoppingBagIcon />
             {cartCount > 0 && (
@@ -97,7 +97,7 @@ export default function Navbar() {
           </Button>
           <Button
             variant="outline"
-            className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none relative"
+            className="hidden lg:flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none relative"
           >
             <FavoriteBorderIcon />
             <span className="hidden text-xs font-semibold text-gray-500 sm:block">
