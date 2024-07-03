c1bac8a2250d44734d8058959bbddc609aed4dbe
Hollali Kelvin <87954725+hollali@users.noreply.github.com>
2024-07-03 14:15:53 +0000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,6 @@ export default function RootLayout({
         <CartProvider>
           <Navbar />
           <ShoppingCartModal />
-          <wishlist />
           {children}
         </CartProvider>
         <Footer />
