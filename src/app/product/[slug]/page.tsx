dd62fe289a55a07b569c3430b62deaf05e378efc
Hollali Kelvin <87954725+hollali@users.noreply.github.com>
2024-07-05 10:30:51 +0000

    Update page.tsx

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,35 +1,36 @@
 import { client } from '@/lib/sanity';
 import CheckoutNow from '@/components/CheckoutNow';
 import { fullProduct } from '@/app/interface';
-import ImageGallery from '@/components/imageGallery';
-import { Button} from "@/components/ui/button";
+import ImageGallery from '@/components/ImageGallery'; // Ensure the path and name are correct
+import { Button } from "@/components/ui/button";
 import { Star, Truck } from 'lucide-react';
 import AddToBag from '@/components/AddToBag';
 
-async function getData(slug: string){
-    const query = *[_type == "product" && slug.current == "${slug}"][0]{
+async function getData(slug: string) {
+    const query = `*[_type == "product" && slug.current == $slug][0]{
         _id,
-          images,
-          price,
-          name,
-          description,
-          "slug": slug.current,
-          "categoryName": category->name,
-          price_id
-      };
+        images,
+        price,
+        name,
+        description,
+        "slug": slug.current,
+        "categoryName": category->name,
+        price_id
+    }`;
 
-    const data = await client.fetch(query);
+    const data = await client.fetch(query, { slug });
 
     return data;
 }
+
 export const dynamic = "force-dynamic";
 
-export default async function ProductPge({
+export default async function ProductPage({
     params,
-}:{
+}: {
     params: { slug: string };
 }) {
-    const data:fullProduct = await getData(params.slug);
+    const data: fullProduct = await getData(params.slug);
     const cedisSign = '\u20B5';
     return (
         <div className="bg-white">
@@ -43,12 +44,12 @@ export default async function ProductPge({
                                 {data.name}
                             </h2>
                         </div>
-                        <div className='mb-6 flex item center gap-3 md:mb-10'>
+                        <div className='mb-6 flex items-center gap-3 md:mb-10'>
                             <Button className='rounded-full gap-x-2'>
                                 <span className='text-sm'>4.5</span>
-                                <Star className='h-5 w-5'/>
+                                <Star className='h-5 w-5' />
                             </Button>
-                            <span className="text-sm text-gray-500 transition duration">56 Ratings</span>
+                            <span className="text-sm text-gray-500">56 Ratings</span>
                         </div>
                         <div className='mb-4'>
                             <div className='flex items-end gap-2'>
@@ -56,10 +57,11 @@ export default async function ProductPge({
                                 <span className="mb-0.5 text-red-500 line-through">{cedisSign} {data.price + 20}</span>
                             </div>
                             <span className='text-sm text-gray-500'>
-                                Incl. Vat. shipping is different</span>
+                                Incl. VAT. Shipping is different
+                            </span>
                         </div>
                         <div className="mb-6 flex items-center gap-2 text-gray-500">
-                            <Truck className='h-6 w-6'/>
+                            <Truck className='h-6 w-6' />
                             <span className="text-sm">4-6 Days delivery</span>
                         </div>
                         <div className='flex gap-2.5'>
@@ -69,7 +71,7 @@ export default async function ProductPge({
                                 image={data.images[0]}
                                 name={data.name}
                                 price={data.price}
-                                key={data._id} 
+                                key={data._id}
                                 price_id={data.price_id}
                             />
                             <CheckoutNow
@@ -78,7 +80,7 @@ export default async function ProductPge({
                                 image={data.images[0]}
                                 name={data.name}
                                 price={data.price}
-                                key={data._id} 
+                                key={data._id}
                                 price_id={data.price_id}
                             />
                         </div>
